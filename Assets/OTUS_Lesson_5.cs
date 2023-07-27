f3a8499c02792c897095cfbe0dd692981102291d
Nikita Kiselev <nikita.k@danko.team>
2023-07-28 02:16:05 +0700

    C1: first commit

---
+++ Assets/OTUS_Lesson_5.cs
@@ -0,0 +1,161 @@
+using System;
+using System.IO;
+using System.Text;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class OTUS_Lesson_5 : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI debugText;
+    
+    private bool isIntArray = true;
+    private int[] intArray;
+    private float[] floatArray;
+
+    private string stringResult;
+    
+    private string saveFileName = "StringResult.txt";
+
+    public void SetBool()
+    {
+        isIntArray = !isIntArray;
+    }
+    public void GetStart(bool isIntArray)
+    {
+
+        //Блок try-catch-finally
+        try
+        {
+            if (isIntArray)
+            {
+                intArray = CreateArray<int>();
+                intArray = FillArray(intArray);
+            }
+            else
+            {
+                floatArray = CreateArray<float>();
+                floatArray = FillArray(floatArray);
+            }
+        }
+        catch
+        {
+            //Ловим exception
+            Debug.LogException(new Exception("Index Out Of Range!!!"));
+        }
+        finally
+        {
+            Debug.Log("CreateArray Completed!");
+
+            if (isIntArray)
+            {
+                LogArray(intArray);
+                
+                //Используем ref
+                CalculateRef(ref intArray[0]);
+                
+                //Используем out
+                CalculateOut(intArray[0], intArray[1], out int sum, out int substract);
+                Debug.Log($"{intArray[0]} + {intArray[1]} = {sum}");
+                Debug.Log($"{intArray[0]} - {intArray[1]} = {substract}");
+
+                ConvertToString(intArray);
+            }
+            else
+            {
+                LogArray(floatArray);
+                //Используем ref
+                CalculateRef(ref floatArray[0]);
+                
+                //Используем out
+                CalculateOut(floatArray[0], floatArray[1], out float sum, out float substract);
+                Debug.Log($"{floatArray[0]} + {floatArray[1]} = {sum}");
+                Debug.Log($"{floatArray[0]} - {floatArray[1]} = {substract}");
+                
+                ConvertToString(floatArray);
+            }
+        }
+        
+    }
+    private T[] CreateArray<T>()
+    {
+        T[] array = new T[Random.Range(5, 10)];
+
+        debugText.text = "Array created!";
+
+        return array;
+    }
+    private T[] FillArray<T>(T[] array)
+    {
+        Type type = typeof(T);
+
+        if (type == typeof(int))
+        {
+            array[0] = (T)(object)Random.Range(2, 10);
+        }
+        else if (type == typeof(float))
+        {
+            array[0] = (T)(object)Random.Range(2.0f, 10.0f);
+        }
+        
+        for (int i = 1; i < array.Length + 1; i++)
+        {
+            dynamic value = array[i - 1];
+            array[i] = (T)(object)(value * value);
+        }
+
+        debugText.text = "Array filled!";
+        
+        return array;
+    }
+    private void LogArray<T>(T[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log($"{typeof(T)} array cell [{i}] value: {array[i]}");
+        }
+
+        debugText.text = "Array logged!";
+    }
+    private void CalculateRef<T>(ref T firstCellValue)
+    {
+        dynamic dynamicFirstCellValue = firstCellValue;
+        dynamicFirstCellValue += 10.0f;
+        firstCellValue = dynamicFirstCellValue;
+        Debug.Log($"new {typeof(T)} array cell [{0}] value: {firstCellValue}");
+
+        debugText.text = "Ref calculated!";
+    }
+    private void CalculateOut<T>(T firstCellValue, T secondCellValue, out T sum, out T substract)
+    {
+        sum = (T)(object)((dynamic)firstCellValue + (dynamic)secondCellValue);
+        substract = (T)(object)((dynamic)firstCellValue - (dynamic)secondCellValue);
+
+        debugText.text = "Out calculated!";
+    }
+    public void ConvertToString<T>(T[] array)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var value in array)
+        {
+            sb.Append(value + "\n");
+        }
+
+        stringResult = sb.ToString();
+
+        debugText.text = "Values converted to string!";
+    }
+
+    public void SaveFile()
+    {
+        string saveFilePath = Application.persistentDataPath + "/" + saveFileName;
+        
+        File.WriteAllText(saveFilePath, stringResult);
+
+        debugText.text = "File saved!";
+        Debug.Log($"Your file saved here: {saveFilePath}");
+    }
+
+}
